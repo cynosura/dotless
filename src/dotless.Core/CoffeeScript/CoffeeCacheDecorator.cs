75b415547b7ebf46e0fd514a9bd4dc6eb2fb87a6
Ray Glover <Ray@NAUSICA>
2011-04-22 16:11:17 +0100

    cache implementation for the coffeescript compiler; works by file timestamps and not md5 checksums for perf.

---
+++ src/dotless.Core/CoffeeScript/CoffeeCacheDecorator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dotless.Core.Loggers;
+using dotless.Core.Cache;
+using System.IO;
+
+namespace dotless.Core.CoffeeScript
+{
+    class CoffeeCacheDecorator : ICoffeeEngine
+    {
+        public readonly ICoffeeEngine Underlying;
+        
+        readonly ICache Cache;
+        readonly ILogger Logger;
+
+        public CoffeeCacheDecorator(ICoffeeEngine underlying, ICache cache)
+            : this(underlying, cache, NullLogger.Instance) { }
+
+        public CoffeeCacheDecorator(ICoffeeEngine underlying, ICache cache, ILogger logger) {
+            Underlying = underlying;
+            Cache = cache;
+            Logger = logger;
+        }
+
+        public string TransformToJavaScript(Stream source, DateTime lastModified, string resourcePath) {
+            //Compute Cache Key
+
+            var timestamp = lastModified.ToBinary();
+            var cacheKey = timestamp.ToString() + resourcePath;
+            
+            if (!Cache.Exists(cacheKey)) {
+                Logger.Debug(String.Format("Inserting cache entry for {0}", cacheKey));
+
+                var css = Underlying.TransformToJavaScript(source, lastModified, resourcePath);
+                var dependancies = new[] { resourcePath };
+
+                Cache.Insert(cacheKey, dependancies, css);
+
+                return css;
+            }
+            Logger.Debug(String.Format("Retrieving cache entry {0}", cacheKey));
+            return Cache.Retrieve(cacheKey);
+        }
+    }
+}
---
+++ src/dotless.Core/Utils/HashUtils.cs
@@ -0,0 +1,22 @@
+﻿namespace dotless.Core.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Security.Cryptography;
+
+    static class HashUtils
+    {
+        public static string ComputeMD5(string source) {
+            return ComputeMD5(System.Text.Encoding.Default.GetBytes(source));
+        }
+
+        public static string ComputeMD5(byte[] source) {
+            using (MD5 md5 = MD5.Create()) {
+                byte[] computeHash = md5.ComputeHash(source);
+                return Convert.ToBase64String(computeHash);
+            }
+        }
+    }
+}
